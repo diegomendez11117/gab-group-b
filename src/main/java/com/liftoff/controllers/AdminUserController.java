229fd75da27f7ac0d7f1a118297f97ab737480eb
madsur <dennissheets@outlook.com>
2021-09-03 10:27:44 -0500

    trying to add the ability to reset password. Added code to the AdminUserController that is not quite working yet

--- src/main/java/com/liftoff/controllers/AdminUserController.java
+++ src/main/java/com/liftoff/controllers/AdminUserController.java
@@ -5,6 +5,7 @@ import com.liftoff.models.User;
 import com.liftoff.models.data.RoleRepository;
 import com.liftoff.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ public class AdminUserController {
     @Autowired
     private RoleRepository roleRepository;
 
+
     @GetMapping ("")
     public String displayManageUsersPage (Model model) {
         List<User> listUsers = userRepository.findAll();
@@ -40,35 +42,60 @@ public class AdminUserController {
 
     @GetMapping ("edit/{id}")
     public String displayEditUserPage(@PathVariable("id") Integer id, Model model){
-        System.out.println("user edit/{id}" + userRepository.getById(id));
+        if (userRepository.existsById(id)) {
             List<User> listUsers = userRepository.findAll();
             List<Role> listRoles = roleRepository.findAll();
             User user = userRepository.findById(id).get();
+            System.out.println("Attempting to edit user: " + user);
             model.addAttribute("listUsers", listUsers);
             model.addAttribute("listRoles", listRoles);
             model.addAttribute("title", "Admin Portal: edit User");
             model.addAttribute("button", "Save Changes");
             model.addAttribute("user", user);
+            System.out.println("Edited user: " + user);
             return "/account/manageUsers/edit";
+        } else {
+            return "/500";
+        }
+    }
 
-
+    @GetMapping("reset/{id}")
+    public String passwordReset (@PathVariable("id") Integer id) {
+        if (userRepository.existsById(id)) {
+            User user = userRepository.getById(id);
+            System.out.println("Attempting to reset password: " + user);
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String resetPassword = "welcome";
+            String encodedPassword = encoder.encode(resetPassword);
+            user.setPassword(encodedPassword);
+            System.out.println("Routing to Save : " + user);
+            return "/account/manageUsers/save}";
+        } else {
+            System.out.println("FAILED TO RESET: USER DOES NOT EXIST");
+            return "/500";
+        }
     }
 
     @GetMapping ("delete/{id}")
-    public String deleteUser (@PathVariable("id") Integer id, User user) {
+    public String deleteUser (@PathVariable("id") Integer id) {
+        User user = userRepository.getById(id);
+        System.out.println("Attempting to delete user: " + user);
         if (userRepository.existsById(id)) {
             userRepository.delete(user);
-            return "redirect:/account/manageUsers/index";
+            System.out.println("Deleted user: " + user);
+            return "/account/manageUsers/index";
         } else {
+            System.out.println("FAILED TO DELETE: USER DOES NOT EXIST");
             return "/500";
         }
     }
 
     @PostMapping("save")
     public String saveUser (User user) {
-        System.out.println("PostMapping('save': " + user);
+        System.out.println("Saved/Updated User : " + user);
         userRepository.save(user);
         return "/account/manageUsers/index";
     }
+
 
 }
