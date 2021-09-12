b6b4821efa49769c9daf240183e72336d558dc36
madsur <dennissheets@outlook.com>
2021-09-12 17:03:07 -0500

    Admin UI Management: Now there are 3 sections, each with a title and a description. at this point the contacts are hard coded

--- src/main/java/com/liftoff/models/Welcome.java
+++ src/main/java/com/liftoff/models/Welcome.java
@@ -2,39 +2,93 @@ package com.liftoff.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Transient;
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
 @Entity
-public class Welcome extends AbstractEntity{
+public class Welcome extends AbstractEntity {
 
-    @Column(name="welcomeParagraph1", columnDefinition="MEDIUMTEXT")
+    @Column(name = "welcomeParagraph1", columnDefinition = "MEDIUMTEXT")
     private String welcomeParagraph1;
 
-    @Column(name="welcomeParagraph2", columnDefinition="MEDIUMTEXT")
+    @Column(name = "welcomeParagraph2", columnDefinition = "MEDIUMTEXT")
     private String welcomeParagraph2;
 
-    @Column(name="welcomeParagraph3", columnDefinition="MEDIUMTEXT")
+    @Column(name = "welcomeParagraph3", columnDefinition = "MEDIUMTEXT")
     private String welcomeParagraph3;
 
-    @Column(name="about1", columnDefinition="MEDIUMTEXT")
-    private String about1;
 
-    @Column(name="about2", columnDefinition="MEDIUMTEXT")
-    private String about2;
 
-    @Column(name="about3", columnDefinition="MEDIUMTEXT")
-    private String about3;
+    @Column(name = "about1Title")
+    private String about1Title;
 
-    @Column(name="highPriorityDescription", columnDefinition="MEDIUMTEXT")
+    @Column(name = "about1Description", columnDefinition = "MEDIUMTEXT")
+    private String about1Description;
+
+    @Column(name = "about2Title")
+    private String about2Title;
+
+    @Column(name = "about2Description", columnDefinition = "MEDIUMTEXT")
+    private String about2Description;
+
+    @Transient
+    String[] developers
+            = { "Dennis Sheets: backend - Java & MySQL", "Enrique Mendez Roger: frontend - BootStrap, HTML,& CSS"};
+
+    @Column(name = "about3Title")
+    private String about3Title;
+
+    @Column(name = "about3Description", columnDefinition = "MEDIUMTEXT")
+    private String about3Description;
+
+
+    @Transient
+    String[] organizations
+            = { "Organization ABD: contact ABC", "Organization 123: contact 123", "Organization!@#: contact!@#"};
+
+
+    @Column(name = "highPriorityTitle")
+    private String highPriorityTitle;
+
+    @Column(name = "highPriorityDescription", columnDefinition = "MEDIUMTEXT")
     private String highPriorityDescription;
 
-    @Column(name="mediumPriorityDescription", columnDefinition="MEDIUMTEXT")
+    @Column(name = "mediumPriorityTitle")
+    private String mediumPriorityTitle;
+
+    @Column(name = "mediumPriorityDescription", columnDefinition = "MEDIUMTEXT")
     private String mediumPriorityDescription;
 
-    @Column(name="lowPriorityDescription", columnDefinition="MEDIUMTEXT")
+    @Column(name = "lowPriorityTitle")
+    private String lowPriorityTitle;
+
+    @Column(name = "lowPriorityDescription", columnDefinition = "MEDIUMTEXT")
     private String lowPriorityDescription;
 
-
     private String mapString;
+
+
+    public Welcome() {}
+
+
+    public String[] getDevelopers() {
+        return developers;
+    }
+
+    public void setDevelopers(String[] developers) {
+        this.developers = developers;
+    }
+
+    public String[] getOrganizations() {
+        return organizations;
+    }
+
+    public void setOrganizations(String[] organizations) {
+        this.organizations = organizations;
+    }
 
     public String getWelcomeParagraph1() {
         return welcomeParagraph1;
@@ -60,28 +114,60 @@ public class Welcome extends AbstractEntity{
         this.welcomeParagraph3 = welcomeParagraph3;
     }
 
-    public String getAbout1() {
-        return about1;
+    public String getAbout1Title() {
+        return about1Title;
     }
 
-    public void setAbout1(String about1) {
-        this.about1 = about1;
+    public void setAbout1Title(String about1Title) {
+        this.about1Title = about1Title;
     }
 
-    public String getAbout2() {
-        return about2;
+    public String getAbout1Description() {
+        return about1Description;
     }
 
-    public void setAbout2(String about2) {
-        this.about2 = about2;
+    public void setAbout1Description(String about1Description) {
+        this.about1Description = about1Description;
     }
 
-    public String getAbout3() {
-        return about3;
+    public String getAbout2Title() {
+        return about2Title;
     }
 
-    public void setAbout3(String about3) {
-        this.about3 = about3;
+    public void setAbout2Title(String about2Title) {
+        this.about2Title = about2Title;
+    }
+
+    public String getAbout2Description() {
+        return about2Description;
+    }
+
+    public void setAbout2Description(String about2Description) {
+        this.about2Description = about2Description;
+    }
+
+    public String getAbout3Title() {
+        return about3Title;
+    }
+
+    public void setAbout3Title(String about3Title) {
+        this.about3Title = about3Title;
+    }
+
+    public String getAbout3Description() {
+        return about3Description;
+    }
+
+    public void setAbout3Description(String about3Description) {
+        this.about3Description = about3Description;
+    }
+
+    public String getHighPriorityTitle() {
+        return highPriorityTitle;
+    }
+
+    public void setHighPriorityTitle(String highPriorityTitle) {
+        this.highPriorityTitle = highPriorityTitle;
     }
 
     public String getHighPriorityDescription() {
@@ -92,12 +178,28 @@ public class Welcome extends AbstractEntity{
         this.highPriorityDescription = highPriorityDescription;
     }
 
+    public String getMediumPriorityTitle() {
+        return mediumPriorityTitle;
+    }
+
+    public void setMediumPriorityTitle(String mediumPriorityTitle) {
+        this.mediumPriorityTitle = mediumPriorityTitle;
+    }
+
     public String getMediumPriorityDescription() {
         return mediumPriorityDescription;
     }
 
     public void setMediumPriorityDescription(String mediumPriorityDescription) {
         this.mediumPriorityDescription = mediumPriorityDescription;
+    }
+
+    public String getLowPriorityTitle() {
+        return lowPriorityTitle;
+    }
+
+    public void setLowPriorityTitle(String lowPriorityTitle) {
+        this.lowPriorityTitle = lowPriorityTitle;
     }
 
     public String getLowPriorityDescription() {
