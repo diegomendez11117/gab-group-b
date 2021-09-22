fe518924d774f443c01e5414929d2308189938fc
Enrique <diegomendez11117@gmail.com>
2021-09-22 12:05:28 -0700

    now the links in the navbar change the active when click in a diferent link

---
+++ src/main/resources/static/js/functions.js
@@ -0,0 +1,19 @@
+ $(function() {
+      if(window.location.pathname == '/'){
+           $('#navbar a').removeClass('active');
+           $('a[href="/"]').addClass('active');
+
+      } else if(window.location.href.indexOf('/wantTo') > -1){
+           $('#navbar a').removeClass('active');
+           $('a[href="/wantTo"]').addClass('active');
+
+        } else if(window.location.href.indexOf('/about') > -1){
+           $('#navbar a').removeClass('active');
+           $('a[href="/about"]').addClass('active');
+
+        } else if(window.location.href.indexOf('/help') > -1){
+           $('#navbar a').removeClass('active');
+           $('a[href="/help"]').addClass('active');
+        }
+
+     });
